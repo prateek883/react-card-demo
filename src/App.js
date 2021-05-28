a7a1c999681979d5a07d1a69afa502de712ca8fd
prateek883 <singhprateek089@gmail.com>
2021-05-28 23:06:35 +0530

    m

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 // import Slider from "./Components/Slider";
-import Create_Blog from "./Components/Create_Blog";
-// import Blog_home from "./Components/Blog_home";
+// import CreateBlog from "./Components/CreateBlog";
+import BlogHome from "./Components/BlogHome";
 // import Blog_card_list from "./Components/Blog_card_list";
 import "./App.css";
 
 function App() {
-  return   <Create_Blog/>
+  return   <BlogHome/>
 }
 
 export default App;
